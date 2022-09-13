fd287a85cedc9204f01d7992c0df10645ee554cc
Ander Mischchaël SAINT-LOUIS <98111836+SAM4you@users.noreply.github.com>
2022-09-13 00:03:07 -0400

    Bottom Navigation ready to be used

--- app/src/main/java/com/slg/G3/sos/MainActivity.java
+++ app/src/main/java/com/slg/G3/sos/MainActivity.java
@@ -1,8 +1,14 @@
 package com.slg.G3.sos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        // Select SOS Fragment
+                        Toast.makeText(MainActivity.this, "SOS", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:return true;
+                    case R.id.action_contacts:
+                        // Select Contacts Fragment
+                        Toast.makeText(MainActivity.this, "Kontak ijans", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_profile:
+                        // Select Profile Fragment
+                        Toast.makeText(MainActivity.this, "Pwofil", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+
+            }
+        }
+
+
+        );
     }
 }
