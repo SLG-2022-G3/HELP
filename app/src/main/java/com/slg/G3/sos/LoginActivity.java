a0f58bfd69ad4fb5b92a9a2e62759c16ddb83f72
Jameson Francisque <francisquejamy@gmail.com>
2022-09-24 04:07:42 -0400

    user can log in

--- app/src/main/java/com/slg/G3/sos/LoginActivity.java
+++ app/src/main/java/com/slg/G3/sos/LoginActivity.java
@@ -3,22 +3,63 @@ package com.slg.G3.sos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "LoginActivity";
 
-    TextView tvLogin;
-    EditText etUsername;
-    EditText etPassword;
+    private TextView tvLogin;
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // user logs in when the button is pressed
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Login button pressed");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username,password);
+
+            }
+        });
     }
 
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "attempt to login: " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "issue with login", e);
+                    return;
+                }
+                Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+
+            }
+
+    });
 
 
+}
 }
