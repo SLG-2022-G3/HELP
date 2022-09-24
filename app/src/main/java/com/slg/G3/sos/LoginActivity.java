07723d4323804b53985bd58f4b80ef818275ce76
Jameson Francisque <francisquejamy@gmail.com>
2022-09-24 03:22:02 -0400

    editText and button added to activity_login.xml

--- app/src/main/java/com/slg/G3/sos/LoginActivity.java
+++ app/src/main/java/com/slg/G3/sos/LoginActivity.java
@@ -3,12 +3,22 @@ package com.slg.G3.sos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+
+    TextView tvLogin;
+    EditText etUsername;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+
 }
