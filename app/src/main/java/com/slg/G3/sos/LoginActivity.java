497224bb6d9e1df176a53bde65a562b9c31d134d
Jameson Francisque <francisquejamy@gmail.com>
2022-09-25 18:14:53 -0400

    Adding code to persist current login user

--- app/src/main/java/com/slg/G3/sos/LoginActivity.java
+++ app/src/main/java/com/slg/G3/sos/LoginActivity.java
@@ -30,6 +30,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
