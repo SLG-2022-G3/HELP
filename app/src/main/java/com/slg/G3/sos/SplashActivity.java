48b7e387b14d1296f31123456a0bdd83a294a56c
Balthazar-02 <laguerrenowsky2@gmail.com>
2022-09-27 02:34:34 -0400

    commit for the design of the login/signup screen

---
+++ app/src/main/java/com/slg/G3/sos/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.slg.G3.sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        imageView = findViewById(R.id.whiteLogo);
+
+        imageView.setAnimation(topAnim);
+
+new Handler().postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        finish();
+    }
+},3000);
+    }
+}
