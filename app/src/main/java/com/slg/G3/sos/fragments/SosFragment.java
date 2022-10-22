fc62bcb4d84ccc19aeff43587789f3001fd9fc6e
Jameson Francisque <francisquejamy@gmail.com>
2022-10-21 22:12:11 -0400

    removing unnecessary comments, to improve code readability/ now using tinydb

--- app/src/main/java/com/slg/G3/sos/fragments/SosFragment.java
+++ app/src/main/java/com/slg/G3/sos/fragments/SosFragment.java
@@ -39,8 +39,10 @@ import com.google.android.gms.tasks.OnTokenCanceledListener;
 import com.google.android.gms.tasks.Task;
 import com.slg.G3.sos.MainActivity;
 import com.slg.G3.sos.R;
+import com.slg.G3.sos.Utils.TinyDB;
 import com.slg.G3.sos.models.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.droidsonroids.gif.GifImageView;
@@ -63,6 +65,7 @@ public class SosFragment extends Fragment {
     private GifImageView btnSOS;
     FusedLocationProviderClient locationProviderClient;
     TextView tvLatitude, tvLongitude;
+    ArrayList<String> phoneContact;
 
 
     public SosFragment() {
@@ -95,6 +98,7 @@ public class SosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sos, container, false);
 
@@ -108,24 +112,19 @@ public class SosFragment extends Fragment {
         locationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
 
-        // shared preferences to retrieve the number of the last
-        //contact the user saved and their name as well
-
-        SharedPreferences sp = getContext().getSharedPreferences("MyContacts", Context.MODE_PRIVATE);
-        String nameContact = sp.getString("contactName", "");
-        String phoneContact = sp.getString("contactPhone", "");
-
-
-        // shared preferences to retrieve the predefined sos message from profile fragment
+       // shared preferences to retrieve the predefined sos message from profile fragment
         SharedPreferences prefs = getContext().getSharedPreferences("Message", Context.MODE_PRIVATE);
         String sosPredefined = prefs.getString("predefMessage", "");
 
+       // using tinyDb class to retrieve list of strings in shared preferences
+        TinyDB tinyDB = new TinyDB(getContext());
+        phoneContact = tinyDB.getListString("Contacts");
 
 
 
+        // click to send SOS
 
         btnSOS = view.findViewById(R.id.btnSOS);
-
 
         btnSOS.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("MissingPermission")
@@ -148,12 +147,12 @@ public class SosFragment extends Fragment {
                     public void onSuccess(Location location) {
                         if (location != null){
                             // Code to Send SOS MESSAGE
-                            String sos = nameContact + sosPredefined + "http://maps.google.com/?q=" + location.getLatitude()  + ","+ location.getLongitude();
+                            String sos = sosPredefined + "http://maps.google.com/?q=" + location.getLatitude()  + ","+ location.getLongitude();
                             if (checkPermission()) {
                                 //Get the default SmsManager//
                                 SmsManager smsManager = SmsManager.getDefault();
                                 //Send the SOS
-                                smsManager.sendTextMessage(phoneContact, null, sos, null, null);
+                                smsManager.sendTextMessage(String.valueOf(phoneContact), null, sos, null, null);
                                 Toast.makeText(getContext(), "Sending SOS", Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
@@ -161,12 +160,12 @@ public class SosFragment extends Fragment {
                             }
 
                         }else {
-                            String sosMessage = "HEY " + nameContact + sosPredefined + "GPS off, No Location provided ";
+                            String sosMessage = sosPredefined + "GPS off, No Location provided ";
                             if (checkPermission()) {
                                 //Get the default SmsManager//
                                 SmsManager smsManager = SmsManager.getDefault();
                                 //Send the SOS
-                                smsManager.sendTextMessage(phoneContact, null, sosMessage, null, null);
+                                smsManager.sendTextMessage(String.valueOf(phoneContact), null, sosMessage, null, null);
                                 Toast.makeText(getContext(), "Sending SOS, without Location", Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
@@ -178,12 +177,12 @@ public class SosFragment extends Fragment {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         // Code to Send SOS MESSAGE
-                        String sosMessage = "HEY " + nameContact + sosPredefined + "GPS off, No Location Provided" ;
+                        String sosMessage = sosPredefined + "GPS off, No Location Provided" ;
                         if (checkPermission()) {
                             //Get the default SmsManager//
                             SmsManager smsManager = SmsManager.getDefault();
                             //Send the SOS
-                            smsManager.sendTextMessage(phoneContact, null, sosMessage, null, null);
+                            smsManager.sendTextMessage(String.valueOf(phoneContact), null, sosMessage, null, null);
                             Toast.makeText(getContext(), "Sending SOS", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
@@ -228,138 +227,5 @@ public class SosFragment extends Fragment {
         ActivityCompat.requestPermissions(getActivity(),
                 new String[]{Manifest.permission.SEND_SMS},MY_PERMISSIONS_REQUEST_SEND_SMS);
     }
-    /*  @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Check condition
-        if (requestCode == 100 && (grantResults.length > 0) && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)){
-            // When permission are granted
-            // Call  method
-            getCurrentLocation();
-        } else {
-            // When permission are denied
-            // Display toast
-            Toast.makeText(getActivity(),
-                            "Permission to access device location denied",
-                            Toast.LENGTH_SHORT)
-                    .show();
-        }
-    }
 
-    @SuppressLint("MissingPermission")
-    private void getCurrentLocation() {
-
-        // Initialize Location manager
-        LocationManager locationManager = (LocationManager)getActivity()
-                .getSystemService(Context.LOCATION_SERVICE);
-        // Check condition
-        if ((locationManager.isProviderEnabled(
-                LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(
-                LocationManager.NETWORK_PROVIDER))) {
-
-                // When location service is enabled
-                // Get last location
-           /* locationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    //Init location
-                    Location location = task.getResult();
-                    //Check Condition
-                    if (location != null){
-                        // When location result is not null set latitude
-                        tvLatitude.setText(
-                                String.valueOf(
-                                        location
-                                                .getLatitude()));
-                        // set longitude
-                        tvLongitude.setText(
-                                String.valueOf(
-                                        location
-                                                .getLongitude()));
-                    } else {
-                    // When location result is null init location request
-                        LocationRequest locationRequest
-                                = new LocationRequest()
-                                .setPriority(
-                                        LocationRequest
-                                                .PRIORITY_HIGH_ACCURACY)
-                                .setInterval(10000)
-                                .setFastestInterval(
-                                        1000)
-                                .setNumUpdates(1);
-
-                        // Initialize location call back
-                        LocationCallback
-                                locationCallback
-                                = new LocationCallback() {
-                            @Override
-                            public void
-                            onLocationResult(
-                                    LocationResult
-                                            locationResult)
-                            {
-                                // Initialize
-                                // location
-                                Location location1
-                                        = locationResult
-                                        .getLastLocation();
-                                // Set latitude
-                                tvLatitude.setText(
-                                        String.valueOf(
-                                                location1
-                                                        .getLatitude()));
-                                // Set longitude
-                                tvLongitude.setText(
-                                        String.valueOf(
-                                                location1
-                                                        .getLongitude()));
-                            }
-                        };
-                        // Request location updates
-                        locationProviderClient.requestLocationUpdates(
-                                locationRequest,
-                                locationCallback,
-                                Looper.myLooper());
-
-                    }
-                }
-            });
-        }
-        else {
-            // When location service is not enable open location setting
-            startActivity(
-                    new Intent(
-                            Settings
-                                    .ACTION_LOCATION_SOURCE_SETTINGS)
-                            .setFlags(
-                                    Intent.FLAG_ACTIVITY_NEW_TASK));
-        }
-
-    }
-
-  /*  protected void sendSOS() {
-        //TODO: Code that retrieve the Predefined SOSMessage, and Emergency Contacts' Number
-
-        //Check Permissions
-
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getContext(), Manifest.permission.SEND_SMS)) {
-
-            } else {
-                ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
-            }
-        }
-    }
-
-
-     private boolean checkPermission(){
-        int result = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS);
-        if(result == PackageManager.PERMISSION_GRANTED){
-            return true;
-        } else {
-            return false;
-        }
-
-    */
 }
