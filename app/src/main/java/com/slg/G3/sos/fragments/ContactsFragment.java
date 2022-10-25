b78f58aa0c4d05ebfadef671c467f0239911267a
Jameson Francisque <francisquejamy@gmail.com>
2022-10-25 16:07:05 -0400

    new Database adapter used in Fragment

--- app/src/main/java/com/slg/G3/sos/fragments/ContactsFragment.java
+++ app/src/main/java/com/slg/G3/sos/fragments/ContactsFragment.java
@@ -38,13 +38,15 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.slg.G3.sos.ContactModel;
 import com.slg.G3.sos.CreateContactActivity;
+import com.slg.G3.sos.DbHelper;
 import com.slg.G3.sos.DetailedContact;
 import com.slg.G3.sos.MainActivity;
 import com.slg.G3.sos.R;
 //import com.slg.G3.sos.adapters.ContactAdapter;
 import com.slg.G3.sos.Utils.TinyDB;
-import com.slg.G3.sos.adapters.ContactAdapter;
+import com.slg.G3.sos.adapters.DatabaseAdapter;
 import com.slg.G3.sos.models.Contact;
 //import com.slg.G3.sos.adapters.ContactAdapter;
 
@@ -63,13 +65,10 @@ public class ContactsFragment extends Fragment {
     private static final int PERMISSION_REQUEST_CODE = 1;
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
 
-
-    private RecyclerView rvEmerServContacts;
-    protected List<Contact> allcontact;
-    protected ContactAdapter contactAdapter;
-    ArrayList<String> phoneContact;
-
-
+    private List<ContactModel> contactModelList;
+    private DbHelper dbHelper;
+    private DatabaseAdapter databaseAdapter;
+    private RecyclerView rvContact;
 
 
 
@@ -94,24 +93,32 @@ public class ContactsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        dbHelper = new DbHelper(getContext());
+
+
 
         RecyclerView rvContacts = view.findViewById(R.id.rvContacts);
 
         //Create Data Source
-        allcontact = new ArrayList<>();
+        contactModelList = new ArrayList<>();
+        contactModelList = dbHelper.getAllContacts();
+
 
         //Create ContactsAdapter
-        contactAdapter = new ContactAdapter(this, allcontact);
 
-        //Set adapter on recyclerview
-        rvContacts.setAdapter(contactAdapter);
+        databaseAdapter = new DatabaseAdapter(getContext(), (ArrayList<ContactModel>) contactModelList);
 
-        //Set a Layout Manager
+
+        //Set layout manager on recyclerview
         rvContacts.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
+        //Set adapter
+        rvContacts.setAdapter(databaseAdapter);
 
-        queryContacts();
+
+
+//        queryContacts();
 
 
   /*      rvEmerServContacts = view.findViewById(R.id.rvEmergServContacts);
@@ -135,157 +142,158 @@ public class ContactsFragment extends Fragment {
         });
 
 
-        // shared preferences to retrieve the number of the last
-        //contact the user saved and their name as well
-
-
-        // using tinyDb class to retrieve list of strings in shared preferences
-        TinyDB tinyDB = new TinyDB(getContext());
-        phoneContact = tinyDB.getListString("Contacts");
-
-
-
-        // shared preferences to retrieve the predefined sos message from profile fragment
-        SharedPreferences prefs = getContext().getSharedPreferences("Message", Context.MODE_PRIVATE);
-        String sosPredefined = prefs.getString("predefMessage", "");
+//        // shared preferences to retrieve the number of the last
+//        //contact the user saved and their name as well
+//
+//
+//        // using tinyDb class to retrieve list of strings in shared preferences
+//        TinyDB tinyDB = new TinyDB(getContext());
+//        phoneContact = tinyDB.getListString("Contacts");
 
 
 
-
-        btnSOS.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
-            @Override
-            public void onClick(View view) {
-                FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-                locationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, new CancellationToken() {
-                    @Override
-                    public boolean isCancellationRequested() {
-                        return false;
-                    }
-                    @NonNull
-                    @Override
-                    public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                        return null;
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null){
-                            // Code to Send SOS MESSAGE
-                            String sos = sosPredefined + "http://maps.google.com/?q=" + location.getLatitude()  + ","+ location.getLongitude();
-                            //String phoneNo = "40770750";
-                            //TODO: retrieve Emergency Contact PhoneNumber to String
-                            if (checkPermission()) {
-                                //Get the default SmsManager//
-                                SmsManager smsManager = SmsManager.getDefault();
-                                //Send the SOS
-                                smsManager.sendTextMessage(String.valueOf(phoneContact), null, sos, null, null);
-                                Toast.makeText(getContext(), "Sending SOS", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
-                                requestPermission();
-                            }
-
-                        }else {
-                            String sosMessage = sosPredefined + "GPS off, No Location provided ";
-                            if (checkPermission()) {
-                                //Get the default SmsManager//
-                                SmsManager smsManager = SmsManager.getDefault();
-                                //Send the SOS
-                                smsManager.sendTextMessage(String.valueOf(phoneContact), null, sosMessage, null, null);
-                                Toast.makeText(getContext(), "Sending SOS, without Location", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
-                                requestPermission();
-                            }
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Code to Send SOS MESSAGE
-                        String sosMessage = sosPredefined + "GPS off, No Location Provided" ;
-                        //TODO: retrieve Emergency Contact PhoneNumber to String
-                        if (checkPermission()) {
-                            //Get the default SmsManager//
-                            SmsManager smsManager = SmsManager.getDefault();
-                            //Send the SOS
-                            smsManager.sendTextMessage(String.valueOf(phoneContact), null, sosMessage, null, null);
-                            Toast.makeText(getContext(), "Sending SOS", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
-                            requestPermission();
-                        }
-                    }
-                });
-
-
-
-            }
-        });
-    }
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
+//        // shared preferences to retrieve the predefined sos message from profile fragment
+//        SharedPreferences prefs = getContext().getSharedPreferences("Message", Context.MODE_PRIVATE);
+//        String sosPredefined = prefs.getString("predefMessage", "");
+//
+//
+//
+//
+//        btnSOS.setOnClickListener(new View.OnClickListener() {
+//            @SuppressLint("MissingPermission")
+//            @Override
+//            public void onClick(View view) {
+//                FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+//
+//                locationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, new CancellationToken() {
+//                    @Override
+//                    public boolean isCancellationRequested() {
+//                        return false;
+//                    }
+//                    @NonNull
+//                    @Override
+//                    public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+//                        return null;
+//                    }
+//                }).addOnSuccessListener(new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        if (location != null){
+//                            // Code to Send SOS MESSAGE
+//                            String sos = sosPredefined + "http://maps.google.com/?q=" + location.getLatitude()  + ","+ location.getLongitude();
+//                            //String phoneNo = "40770750";
+//                            //TODO: retrieve Emergency Contact PhoneNumber to String
+//                            if (checkPermission()) {
+//                                //Get the default SmsManager//
+//                                SmsManager smsManager = SmsManager.getDefault();
+//                                //Send the SOS
+//                                smsManager.sendTextMessage(String.valueOf(phoneContact), null, sos, null, null);
+//                                Toast.makeText(getContext(), "Sending SOS", Toast.LENGTH_SHORT).show();
+//                            } else {
+//                                Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
+//                                requestPermission();
+//                            }
+//
+//                        }else {
+//                            String sosMessage = sosPredefined + "GPS off, No Location provided ";
+//                            if (checkPermission()) {
+//                                //Get the default SmsManager//
+//                                SmsManager smsManager = SmsManager.getDefault();
+//                                //Send the SOS
+//                                smsManager.sendTextMessage(String.valueOf(phoneContact), null, sosMessage, null, null);
+//                                Toast.makeText(getContext(), "Sending SOS, without Location", Toast.LENGTH_SHORT).show();
+//                            } else {
+//                                Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
+//                                requestPermission();
+//                            }
+//                        }
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        // Code to Send SOS MESSAGE
+//                        String sosMessage = sosPredefined + "GPS off, No Location Provided" ;
+//                        //TODO: retrieve Emergency Contact PhoneNumber to String
+//                        if (checkPermission()) {
+//                            //Get the default SmsManager//
+//                            SmsManager smsManager = SmsManager.getDefault();
+//                            //Send the SOS
+//                            smsManager.sendTextMessage(String.valueOf(phoneContact), null, sosMessage, null, null);
+//                            Toast.makeText(getContext(), "Sending SOS", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(getContext(), "SOS Can not be Sent, access denied", Toast.LENGTH_SHORT).show();
+//                            requestPermission();
+//                        }
+//                    }
+//                });
+//
+//
+//
+//            }
+//        });
     }
 
 
+//    private boolean checkPermission() {
+//        int result = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS);
+//        if (result == PackageManager.PERMISSION_GRANTED) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        switch (requestCode) {
+//            case PERMISSION_REQUEST_CODE:
+//                if (grantResults.length > 0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(getContext(), "Permission access allowed", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(getContext(), "Permission access denied", Toast.LENGTH_SHORT).show();
+//                }
+//                break;
+//        }
+//
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//    }
+//
+//    private void requestPermission(){
+//        ActivityCompat.requestPermissions(getActivity(),
+//                new String[]{Manifest.permission.SEND_SMS},MY_PERMISSIONS_REQUEST_SEND_SMS);
+//    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(getContext(), "Permission access allowed", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getContext(), "Permission access denied", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(getActivity(),
-                new String[]{Manifest.permission.SEND_SMS},MY_PERMISSIONS_REQUEST_SEND_SMS);
-    }
-
-
-    private void queryContacts() {
-        // Specify which class to query
-        ParseQuery<Contact> query = ParseQuery.getQuery("Contact");
-        query.fromLocalDatastore();
-        query.orderByAscending("createdAt");
-        query.setLimit(1);
-        query.whereEqualTo(Contact.KEY_USER, ParseUser.getCurrentUser());
-
-        //Specify the object ID
-        query.findInBackground(new FindCallback<Contact>() {
-            @Override
-            public void done(List<Contact> contacts, ParseException e) {
-                if (e !=null){
-                    Log.e(TAG, "Issues with getting Contacts", e);
-                    return;
-                } for(Contact contact : contacts){
-                    Log.i(TAG, "Contact's name : "+ contact.getName() + "Phone Number : "+ contact.getNumber() + "photo: " + contact.getImage());
-                    contact.saveInBackground();
-                }
-
-                // contacts.clear();
-                allcontact.addAll(contacts);
-                contactAdapter.notifyDataSetChanged();
-            }
-        });
-
-
-
-    }
+//
+//    private void queryContacts() {
+//        // Specify which class to query
+//        ParseQuery<Contact> query = ParseQuery.getQuery("Contact");
+//        query.fromLocalDatastore();
+//        query.orderByAscending("createdAt");
+//        query.setLimit(1);
+//        query.whereEqualTo(Contact.KEY_USER, ParseUser.getCurrentUser());
+//
+//        //Specify the object ID
+//        query.findInBackground(new FindCallback<Contact>() {
+//            @Override
+//            public void done(List<Contact> contacts, ParseException e) {
+//                if (e !=null){
+//                    Log.e(TAG, "Issues with getting Contacts", e);
+//                    return;
+//                } for(Contact contact : contacts){
+//                    Log.i(TAG, "Contact's name : "+ contact.getName() + "Phone Number : "+ contact.getNumber() + "photo: " + contact.getImage());
+//                    contact.saveInBackground();
+//                }
+//
+//                // contacts.clear();
+//
+//            }
+//        });
+//
+//
+//
+//    }
 
 
 
